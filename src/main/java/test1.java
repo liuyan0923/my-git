e653206e777ba233b570cb39b8423bb76edd0ebf
liuyan <827543113@qq.com>
2023-03-07 10:11:13 +0800

    hot-fox first commit

--- src/main/java/test1.java
+++ src/main/java/test1.java
@@ -2,5 +2,6 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("test1111");
         System.out.println("test2222");
+        System.out.println("hot-fox11111");
     }
 }
